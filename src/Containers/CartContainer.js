3381e186bf167303931201ed77545f362e3ad9f6
Stepan <nemail.necom@gmail.com>
2020-12-31 22:26:13 +0300

    started work with cart

---
+++ src/Containers/CartContainer.js
@@ -0,0 +1,10 @@
+// import { connect } from 'react-redux';
+// import BookCard from '../Components//BookCard';
+
+
+
+// const mapState = (state) => ({
+//   booksInCart: state.cartReducer.booksInCart
+// });
+
+// export default connect( mapState, { cartAddAC } )(BookCard);
